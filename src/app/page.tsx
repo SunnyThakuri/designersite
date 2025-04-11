f0b4717a8350341a18b68929498e2097e08fa383
Genious-Bajracharya <102010683+Genious-Bajracharya@users.noreply.github.com>
2025-04-11 15:58:43 +0545

    added instagram highlights

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import LandingServices from "@/components/landing/services";
 import Quote from "@/components/landing/quote";
 import Insights from "@/components/landing/insights";
 import Expert from "@/components/landing/expert";
+import InstagramHighlights from "@/components/landing/instagram";
 
 export default function Home() {
   return (
@@ -10,8 +11,9 @@ export default function Home() {
       <LandingHero/>
       <LandingServices/>
       <Quote/>
-      <Insights/>
       <Expert/>
+      <Insights/>
+      <InstagramHighlights/>
     </div>
   );
 }
---
+++ src/components/landing/instagram.tsx
@@ -0,0 +1,124 @@
+import Image from 'next/image'
+import React from 'react'
+
+const InstagramHighlights = () => {
+  return (
+    <div className="my-12 lg:my-[140px] w-full">
+      <h1 className="text-center text-[22px] mb-6">INSTAGRAM HIGHLIGHTS</h1>
+      <div className='flex flex-col md:flex-row gap-3 w-full py-6 px-4 md:px-0'>
+        {/* Left Side */}
+        <div className='flex flex-col w-full md:w-[40%] gap-3 '>
+          {/* Left1 */}
+          <div className='flex gap-3 '>
+            <div className=" md:h-[405px] w-full">
+              <Image
+                src="/images/landing/service1.jpg"
+                width={270}
+                height={405}
+                alt="instagram-photo"
+                className="rounded-lg w-full h-full object-cover"
+              />
+            </div>
+            <div className="md:h-[325px] w-full self-end">
+              <Image
+                src="/images/landing/service1.jpg"
+                width={270}
+                height={325}
+                alt="instagram-photo"
+                className="rounded-lg w-full h-full object-cover"
+              />
+            </div>
+          </div>
+
+          {/* Left2 */}
+          <div className='flex gap-3 w-full overflow-hidden'>
+            <div className=" md:h-[240px] md:w-[317px] ">
+              <Image
+                src="/images/landing/service1.jpg"
+                width={317}
+                height={240}
+                alt="instagram-photo"
+                className="rounded-lg w-full h-full object-cover "
+              />
+            </div>
+            <div className="md:h-[200px] md:w-[317px] lg:w-full  ">
+              <Image
+                src="/images/landing/service1.jpg"
+                width={317}
+                height={200}
+                alt="instagram-photo"
+                className="rounded-lg w-full h-full object-cover"
+              />
+            </div>
+          </div>
+        </div>
+
+        {/* Middle */}
+        <div className='grid items-center justify-center w-full md:w-[20%]'>
+        <div className="md:h-[390px] w-full">
+              <Image
+                src="/images/landing/service1.jpg"
+                alt="instagram"
+                width={324}
+                height={390}
+                className="rounded-lg w-full h-full object-cover"
+              />
+            </div>
+        </div>
+
+        {/* Right */}
+        <div className='flex flex-col w-full md:w-[40%] gap-3 '>
+          {/* Left1 */}
+          <div className='w-full flex gap-3'>
+            <div className=" md:h-[325px] w-full self-end">
+              <Image
+                src="/images/landing/service1.jpg"
+                width={270}
+                height={405}
+                alt="instagram-photo"
+                className="rounded-lg w-full h-full object-cover"
+              />
+            </div>
+            <div className="md:h-[405px] w-full">
+              <Image
+                src="/images/landing/service1.jpg"
+                width={270}
+                height={325}
+                alt="instagram-photo"
+                className="rounded-lg w-full h-full object-cover"
+              />
+            </div>
+          </div>
+
+          {/* Left2 */}
+          <div className='flex gap-3 '>
+          <div className="w-full md:w-[200px] md:h-[186px]">
+              <Image
+                src="/images/landing/service1.jpg"
+                width={270}
+                height={405}
+                alt="instagram-photo"
+                className="rounded-lg w-full h-full object-cover"
+              />
+            </div>
+            <div className="md:h-[240px] w-full">
+              <Image
+                src="/images/landing/service1.jpg"
+                width={270}
+                height={325}
+                alt="instagram-photo"
+                className="rounded-lg w-full h-full object-cover"
+              />
+            </div>
+          </div>
+        </div>
+
+      </div>
+      <div className='grid items-center mx-auto'>
+        <button className='shop-btn-dark mx-auto'>VIEW INSTAGRAM</button>
+      </div>
+    </div>
+  );
+};
+
+export default InstagramHighlights;
