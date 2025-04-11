cfc4c6bfe03373eb040f39780267d225b8197fb3
Genious-Bajracharya <102010683+Genious-Bajracharya@users.noreply.github.com>
2025-04-11 14:23:51 +0545

    added quote page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import LandingHero from "@/components/landing/hero";
 import LandingServices from "@/components/landing/services";
+import Quote from "@/components/landing/quote";
+
 export default function Home() {
   return (
     <div className="space-y-[160px]">
       <LandingHero/>
       <LandingServices/>
+      <Quote/>
     </div>
   );
 }
---
+++ src/components/landing/quote.tsx
@@ -0,0 +1,40 @@
+import Image from "next/image"
+
+const Quote = () =>{
+    return(
+        <div className="plr">
+            <div className="bg-[#F3FAFF] flex gap-[100px] p-14">
+                <div className="space-y-[48px]">
+                    <p className="text-[40px] font-semibold">Get Free Quotes</p>
+                    <div className="space-y-8">
+                        <input 
+                            placeholder="Full Name"
+                            className="bg-white py-2 rounded-lg w-full px-4" />
+                            <input 
+                            placeholder="Full Name"
+                            className="bg-white py-2 rounded-lg w-full px-4" />
+                            <input 
+                            placeholder="Full Name"
+                            className="bg-white py-2 rounded-lg w-full px-4" />
+                            <input 
+                            placeholder="Full Name"
+                            className="bg-white py-2 rounded-lg w-full px-4" />
+                    </div>
+                    <button className="px-6 text-white py-2 bg-blue-700 rounded-xl">
+                        Submit
+                    </button>
+                </div>
+                <div className="w-full h-[600px]">
+                    <Image
+                    src={"/images/landing/service1.jpg"}
+                    alt="img"
+                    height={600}
+                    width={500}
+                    className="w-full h-full object-cover rounded-lg"/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Quote
