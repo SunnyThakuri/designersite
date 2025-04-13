0326b956f676766891e57ac4113d751cf0bb1f3c
Genious-Bajracharya <102010683+Genious-Bajracharya@users.noreply.github.com>
2025-04-13 16:26:11 +0545

    added pages in about us page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,17 @@
 import AboutHero from "@/components/about/abouthero";
 import Welcome from "@/components/about/welcome";
+import LandingFaq from "@/components/landing/faq";
+import OurWork from "@/components/landing/works";
+import Expert from "@/components/landing/expert";
+
 const About = () =>{
     return(
-        <div>
+        <div className="lg:space-y-[160px]">
             <AboutHero/>
             <Welcome/>
+            <Expert/>
+            <LandingFaq/>
+            <OurWork/>
         </div>
     )
 }
