865f61f25d60d789900faf8520a8dc96023a6bf2
Genious-Bajracharya <102010683+Genious-Bajracharya@users.noreply.github.com>
2025-04-13 12:11:20 +0545

    updated footer

--- src/components/common/footer/footer.tsx
+++ src/components/common/footer/footer.tsx
@@ -1,14 +1,17 @@
+import { FiPhone } from "react-icons/fi";
+import { CiLocationOn, CiMail } from "react-icons/ci";
+import { FaInstagram, FaFacebook } from "react-icons/fa";
 const Footer = () =>{
     return(
-        <div className="flex justify-between plr">
+        <div className="flex justify-between flex-wrap plr lg:mt-[160px] lg:py-[54px]">
             {/* Logo */}
             <div>
-
+                logo
             </div>
 
-            <div>
-                <p className="text-xl font-semi-bold">Quick Links</p>
-                <div className="text-slate-300">
+            <div className="space-y-5">
+                <p className="text-xl font-semibold">Quick Links</p>
+                <div className="text-[#383838] space-y-2">
                     <p>Home</p>
                     <p>About</p>
                     <p>Services</p>
@@ -17,9 +20,9 @@ const Footer = () =>{
                 </div>
             </div>
 
-            <div>
-                <p className="text-xl font-semi-bold">Services</p>
-                <div>
+            <div className="space-y-5">
+                <p className="text-xl font-semibold">Services</p>
+                <div className="text-[#383838] space-y-2">
                     <p>Gutter Cleaning</p>
                     <p>Office Cleaning</p>
                     <p>Home Cleaning</p>
@@ -31,10 +34,25 @@ const Footer = () =>{
                 </div>
             </div>
 
-            <div>
-                <p>Contact</p>
-                <div>
-                    
+            <div className="space-y-5">
+                <p className="text-xl font-semibold">Contact</p>
+                <div className="space-y-2">
+                    <div className="flex items-center gap-4">
+                        <FiPhone className="w-5 h-5"/>
+                        <p>+61 416 942 392</p>
+                    </div>
+                    <div className="flex items-center gap-4">
+                        <CiLocationOn className="w-5 h-5"/>
+                        <p>Sydney, Australia</p>
+                    </div>
+                    <div className="flex items-center gap-4">
+                        <CiMail className="w-5 h-5"/>
+                        <p>info@interniordesign.com</p>
+                    </div>
+                </div>
+                <div className="flex items-center gap-4">
+                    <FaFacebook className="w-5 h-5"/>
+                    <FaInstagram className="w-5 h-5"/>
                 </div>
             </div>
         </div>
