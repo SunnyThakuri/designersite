39ba687b110c6ba327d09fc94b92da7c69c82fd4
Genious-Bajracharya <102010683+Genious-Bajracharya@users.noreply.github.com>
2025-04-17 14:30:25 +0545

    added new stone designs page

--- src/components/stonedesign/stoneproducts.tsx
+++ src/components/stonedesign/stoneproducts.tsx
@@ -1,12 +1,15 @@
 import Image from "next/image"
 
 const StoneProducts = () =>{
-    
+    const designs =[1,2,3,4,5,6,7,8,9]
     
     return(
         <div className="grid grid-cols-3 gap-4">
             {/* card */}
-            <div className="relative">
+            {
+                designs.map((design,index)=>(
+                    
+            <div key={index} className="relative">
                 <div className="w-full h-[400px]">
                     <Image
                     src={"/images/landing/design1.webp"}
@@ -17,6 +20,9 @@ const StoneProducts = () =>{
                 </div>
                 <p className="bg-white/70 px-4  absolute bottom-1 ">Purple Diamond</p>
             </div>
+                ))
+            }
+
         </div>
     )
 }
