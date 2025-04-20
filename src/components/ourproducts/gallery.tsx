37b6c73ea6d525a16b45d04e3c766e4f02cccba1
Genious-Bajracharya <102010683+Genious-Bajracharya@users.noreply.github.com>
2025-04-20 13:44:27 +0545

    updated stonedesigns page

--- src/components/ourproducts/gallery.tsx
+++ src/components/ourproducts/gallery.tsx
@@ -1,35 +1,49 @@
 import Image from "next/image"
 
 const Gallery  = () =>{
-    const designs =[1,2,3,4,5,6,7,8,9]
+    const designs =[1,2,3]
+    const design2 = [1,2]
 
     return(
-        <div className="space-y-[60px]">
-            <div className="plr text-center space-y-6">
-            <p className="text-3xl font-medium">Inspiration Gallery</p>
-            <p className="">Find your style with our quartz countertops. Explore our <br /> impressive design palette, featuring 
-                unique and refined styles that are crafted <br />to be as enduring as they are beautiful.</p>
-        </div>
-        <div className="grid grid-cols-3 gap-4">
-                    {/* card */}
+        <div className="space-y-[40px]">
+            <div className="plr text-center space-y-4">
+                <p className="text-3xl font-medium">Inspiration Gallery</p>
+                <p className="">Find your style with our quartz countertops. Explore our <br /> impressive design palette, featuring 
+                    unique and refined styles that are crafted <br />to be as enduring as they are beautiful.</p>
+            </div>
+            <div className="space-y-4">
+                <div className="grid grid-cols-3 gap-4">
                     {
-                        designs.map((design,index)=>(
+                        designs.map((item,index) =>(
+                            <div key={index} className="w-full h-[500px]">
+                                <Image
+                                    src={"/images/landing/design1.webp"}
+                                    alt="img"
+                                    width={500}
+                                    height={500}
+                                    className="w-full h-full object-cover"/>
+                            </div>
                             
-                    <div key={index} className="relative">
-                        <div className="w-full h-[400px]">
-                            <Image
-                            src={"/images/landing/design1.webp"}
-                            alt="img"
-                            width={500}
-                            height={500}
-                            className="w-full h-full object-cover "/>
-                        </div>
-                        <p className="bg-white/70 px-4  absolute bottom-1 ">Purple Diamond</p>
-                    </div>
                         ))
                     }
-        
                 </div>
+                <div className="grid grid-cols-2 gap-4">
+                {
+                        design2.map((item,index) =>(
+                            <div key={index} className="w-full h-[500px]">
+                                <Image
+                                    src={"/images/landing/design1.webp"}
+                                    alt="img"
+                                    width={500}
+                                    height={500}
+                                    className="w-full h-full object-cover"/>
+                            </div>
+                            
+                        ))
+                    }
+                </div>
+            </div>
+        
         </div>
     )
 }
