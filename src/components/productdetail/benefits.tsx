6f9262bca7cfc9708781b1a313ac5d9a45e33e9c
Genious-Bajracharya <102010683+Genious-Bajracharya@users.noreply.github.com>
2025-04-21 14:34:19 +0545

    added new compoenent to product details pagfe

--- src/components/productdetail/benefits.tsx
+++ src/components/productdetail/benefits.tsx
@@ -1,25 +1,25 @@
 const Benefits = () =>{
     return(
-        <div className="py-24  space-y-6">
-            <p className="text-center text-2xl">Benefits of </p>
-            <div className="flex justify-between">
+        <div className="py-36 px-24 bg-[#f8f5f0]  space-y-6">
+            <p className="text-center text-2xl text-[#8c6709]">Benefits of </p>
+            <div className="flex gap-14">
                 {/* numero1 */}
-                <div className="flex gap-2">
-                    <p className="text-5xl">1</p>
-                    <p className="h-full w-[1px] border-[1px]"></p>
-                    <p>Every quartz countertop is nonabsorbent, stain resistant, incredibly resilient, and easy to clean</p>
+                <div className="flex gap-4">
+                    <p className="text-[60px] text-[#8c6709]">1</p>
+                    <p className="h-full w-[1px] border-[1px] "></p>
+                    <p className="text-xl">Every quartz countertop is nonabsorbent, stain resistant, incredibly resilient, and easy to clean</p>
                 </div>
                 {/* numero1 */}
-                <div className="flex gap-2">
-                    <p className="text-5xl">2</p>
+                <div className="flex gap-4">
+                    <p className="text-[60px] text-[#8c6709]">2</p>
                     <p className="h-full w-[1px] border-[1px]"></p>
-                    <p>Every quartz countertop is nonabsorbent, stain resistant, incredibly resilient, and easy to clean</p>
+                    <p className="text-xl">Every quartz countertop is nonabsorbent, stain resistant, incredibly resilient, and easy to clean</p>
                 </div>
                 {/* numero1 */}
-                <div className="flex gap-2">
-                    <p className="text-5xl">3</p>
+                <div className="flex gap-4">
+                    <p className="text-[60px] text-[#8c6709]">3</p>
                     <p className="h-full w-[1px] border-[1px]"></p>
-                    <p>Every quartz countertop is nonabsorbent, stain resistant, incredibly resilient, and easy to clean</p>
+                    <p className="text-xl">Every quartz countertop is nonabsorbent, stain resistant, incredibly resilient, and easy to clean</p>
                 </div>
             </div>
         </div>
