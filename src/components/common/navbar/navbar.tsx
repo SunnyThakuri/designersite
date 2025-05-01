08cfbda6a4978d251606143b20dcdf7ac3e5178e
Genious-Bajracharya <102010683+Genious-Bajracharya@users.noreply.github.com>
2025-05-01 17:32:12 +0545

    updatd navbar

--- src/components/common/navbar/navbar.tsx
+++ src/components/common/navbar/navbar.tsx
@@ -93,6 +93,7 @@ import Link from "next/link";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const[dropdown,setDropdown] = useState(false)
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
@@ -100,7 +101,6 @@ const Navbar = () => {
 
   return (
     <>
-      {/* Original Navbar for Larger Screens */}
       <div className="hidden md:block plr absolute z-10 w-full shadow-md">
         <div className="w-full px-4 py-4 rounded-full items-center flex justify-between">
           <div className="h-[50px] w-[200px] items-center grid">
@@ -111,12 +111,11 @@ const Navbar = () => {
             <Link href={"/"}>
               <p>Home</p>
             </Link>
-            <Link href={"/about"}>
+            <Link onMouseEnter={()=> setDropdown(false)} href={"/about"}>
               <p>About</p>
             </Link>
             <div
-              onMouseEnter={() => setIsMenuOpen(true)}
-              onMouseLeave={() => setIsMenuOpen(false)}
+              onMouseEnter={() => setDropdown(true)}
             >
               <Link href={""}>
                 <p>Our Products</p>
@@ -132,10 +131,10 @@ const Navbar = () => {
         </div>
 
         {/* Dropdown Menu for "Our Products" */}
-        {isMenuOpen && (
+        {dropdown && (
           <div
-            onMouseLeave={() => setIsMenuOpen(false)}
-            className="flex gap-10 absolute z-40 top-[80px] pl-32 py-10 justify-center bg-white w-full h-[400px]"
+            onMouseLeave={() => setDropdown(false)}
+            className="flex  gap-10 absolute z-40 left-0 top-[80px] pl-32 py-10 justify-center bg-white w-full h-[400px]"
           >
             <div className="flex flex-col gap-6 text-xl font-medium">
               <Link href={"/ourproducts/stonedesign"}>
